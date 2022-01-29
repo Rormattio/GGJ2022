b9d5b78f9fb29895d7db3eb6f31bc901c8e86043
bensarthou <bsarthou@hotmail.fr>
2022-01-29 22:32:15 +0100

    Transition test for smoothing

--- Transition.gd
+++ Transition.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
+# Path to the next scene to transition to
+#export(String, FILE, "*.tscn") var next_scene_path
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Reference to the _AnimationPlayer_ node
+#onready var _anim_player := $AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Plays the animation backward to fade in
 	$VideoPlayer.play()
+#	_anim_player.play("Fad")
+#	yield(_anim_player, "animation_finished")
 
 
 
@@ -18,4 +22,6 @@ func _ready():
 
 
 func _on_VideoPlayer_finished():
+#	_anim_player.play("Fade")
+#	yield(_anim_player, "animation_finished")
 	get_tree().change_scene("res://Run.tscn")
