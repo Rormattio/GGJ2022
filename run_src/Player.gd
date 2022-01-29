807d4448b6bd954a005bd3ddc7867101cd554a5f
Léop <tremelow@Himawari.local>
2022-01-29 12:12:34 +0100

    minor player fixes

--- run_src/Player.gd
+++ run_src/Player.gd
@@ -17,25 +17,27 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	
 func _process(delta):
+	move_local_x(speed*delta)
 	$AnimatedSprite.play()
 	$AnimatedSprite.animation = "base"
 	
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+#	## Player movement
+#	var velocity = Vector2.ZERO # The player's movement vector.
+#	if Input.is_action_pressed("ui_right"):
+#		velocity.x += 1
+#	if Input.is_action_pressed("move_left"):
+#		velocity.x -= 1
+#	if Input.is_action_pressed("move_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("move_up"):
+#		velocity.y -= 1
+#
+#	if velocity.length() > 0:
+#		velocity = velocity.normalized() * speed
+#
+#	position += velocity * delta
+#	position.x = clamp(position.x, 0, screen_size.x)
+#	position.y = clamp(position.y, 0, screen_size.y)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
