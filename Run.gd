b8b0a8666b1bb4898fdaead05250fc96c7028847
Léop <tremelow@Himawari.local>
2022-01-29 13:03:13 +0100

    parallax structure

--- Run.gd
+++ Run.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 export(PackedScene) var mob_scene
+export var speed = 400 # How fast the player will move (pixels/sec).
 var score
 
 func _ready():
 	randomize()
+
+func _process(delta):
+	pass
+#	move_local_x(-speed*delta)
--- run_src/Player.gd
+++ run_src/Player.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 signal hit
 
@@ -18,24 +18,22 @@ func _ready():
 	
 func _process(delta):
 	move_local_x(speed*delta)
-	$AnimatedSprite.play()
-	$AnimatedSprite.animation = "base"
 	
-#	## Player movement
-#	var velocity = Vector2.ZERO # The player's movement vector.
-#	if Input.is_action_pressed("ui_right"):
-#		velocity.x += 1
-#	if Input.is_action_pressed("move_left"):
-#		velocity.x -= 1
-#	if Input.is_action_pressed("move_down"):
-#		velocity.y += 1
-#	if Input.is_action_pressed("move_up"):
-#		velocity.y -= 1
-#
-#	if velocity.length() > 0:
-#		velocity = velocity.normalized() * speed
-#
-#	position += velocity * delta
+	## Player movement
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 0.5
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+	position += velocity * delta
 #	position.x = clamp(position.x, 0, screen_size.x)
 #	position.y = clamp(position.y, 0, screen_size.y)
 
