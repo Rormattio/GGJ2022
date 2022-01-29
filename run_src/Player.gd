445ccb71f3f1c449c874b781dcb218adc5c8cb8e
bensarthou <bsarthou@hotmail.fr>
2022-01-29 17:15:31 +0100

    Gros commit - run working with obstacles and switch

--- run_src/Player.gd
+++ run_src/Player.gd
@@ -9,7 +9,21 @@ var screen_size # Size of the game window.
 func start(pos):
 	position = pos
 	show()
-	$CollisionShape2D.disabled = false
+	$PlayerParticle/CollisionParticle.set_deferred("disabled", false)
+	$PlayerWave/CollisionWave.set_deferred("disabled", true)
+	$AnimatedSprite.animation = "particle"
+	$AnimationSprite.play()
+
+func change_state():
+	if $AnimatedSprite.animation == "particle":
+		$AnimatedSprite.animation = "wave"
+	elif $AnimatedSprite.animation == "wave":
+		$AnimatedSprite.animation = "particle"
+	$AnimatedSprite.play()
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		change_state()
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,7 +32,13 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	
 func _process(delta):
-	
+	if $AnimatedSprite.animation == "particle":
+		$PlayerParticle/CollisionParticle.set_deferred("disabled", false)
+		$PlayerWave/CollisionWave.set_deferred("disabled", true)
+	elif $AnimatedSprite.animation == "wave":
+		$PlayerParticle/CollisionParticle.set_deferred("disabled", true)
+		$PlayerWave/CollisionWave.set_deferred("disabled", false)
+
 	## Player movement
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
@@ -43,8 +63,9 @@ func _process(delta):
 #	pass
 
 
-func _on_PlayerArea_body_entered(body):
-#	hide() # Player disappears after being hit.
+func _on_PlayerParticle_body_entered(body):
 	emit_signal("hit")
-	# Must be deferred as we can't change physics properties on a physics callback.
-#	$PlayerArea/CollisionShape2D.set_deferred("disabled", true)
+
+
+func _on_PlayerWave_body_entered(body):
+	emit_signal("hit")
