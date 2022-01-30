8d373e92b57f8a0c0dcb106545847ec01d8c100c
Léop <tremelow@Himawari.local>
2022-01-30 15:32:37 +0100

    simplified level generator

--- run_src/level_generator.py
+++ run_src/level_generator.py
@@ -1,40 +1,28 @@
 import numpy as np
+import json
 
-class Obstacle:
-    def __init__(self,timing,obstacleType,position,speed):
-        self.timing = timing
-        self.type = obstacleType
-        self.pos = position
-        self.speed = speed
+def obstacle(obstacleType, position, speed):
+    return {"type": obstacleType,
+            "location": list(position),
+            "speed": speed
+            }
 
-    def _timing_json(self):
-        return '"type": "%s"'%(self.type)
-    def _position_json(self):
-        return '"location": [%d,%d]'%(self.pos[0],self.pos[1])
-    def _speed_json(self):
-        return '"speed": [%d,%d]'%(self.speed[0],self.speed[1])
-    def as_json_object(self):
-        inside = ',\n\t\t'.join([
-                self._timing_json(),self._position_json(),self._speed_json()
-                ])
-        return '\n\t\t'.join(['{',inside,'}'])
-
-obs = []
-t = 3
+t = '3'
+obs = {t: []}
 init_pos = np.array( [1400,500] )
 current_pos = init_pos[:]
 speed = [500,30]
 width = 200
 taxi_xdist = 80
-slope = 30
+slope = 20
 
 taxi_offset = np.array([0,width/2])
-for i in range(5):
-    obs.append( Obstacle(t,"taxi",current_pos - taxi_offset,speed) )
-    obs.append( Obstacle(t,"taxi",current_pos + taxi_offset,speed) )
+for i in range(10):
+    obs[t].append( obstacle("taxi",current_pos - taxi_offset,speed) )
+    obs[t].append( obstacle("taxi",current_pos + taxi_offset,speed) )
     current_pos += np.array([taxi_xdist,slope])
 
-print( ',\n\t\t'.join([o.as_json_object() for o in obs]) )
+print(json.dumps(obs,indent=2))
 
 ast_ymargin = 130
 ast_xdist = 150
